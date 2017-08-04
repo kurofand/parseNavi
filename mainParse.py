27e7c9a0df9055731f35f4d7760deb4beaeba588
EC2 Default User <ec2-user@ip-172-31-7-145.ap-northeast-1.compute.internal>
2017-08-04 04:41:02 +0000

    resolved bug with rewrite file after open next shop

--- mainParse.py
+++ mainParse.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*
 
 import requests
@@ -5,9 +6,14 @@ import json
 from bs4 import BeautifulSoup
 from datetime import datetime
 import time
+import os.path
 
 def getHTML(url):
-	html=requests.get(url)
+	try:
+		html=requests.get(url)
+	except requests.eceptions.ConnectionError:
+		time.sleep(300)
+		html=requests.get(url)
 	data=html.text
 	body=BeautifulSoup(data, 'lxml')
 	return(body)
@@ -17,6 +23,7 @@ shopTypes=['0201', '0202']
 #выдирать значения можно непосредственно из селектов
 #0201 - конбини
 startTime=datetime.now();
+recordCount=0
 for shopType in shopTypes:
 	body=getHTML('https://www.navitime.co.jp/category/%s'%shopType)
 	shops=body.find('select', {'id':'detail_category_id_select'}).findChildren('option')
@@ -25,17 +32,22 @@ for shopType in shopTypes:
 			body=getHTML('https://www.navitime.co.jp/category/%s'%shop['value'])
 			selectPref=body.find('select', {'id':'prefecture_select'})
 			prefList=selectPref.findChildren('option')
-			recordCount=0
 			for prefecture in prefList:
 				#сейчас только Токио, поэтому только 13
 				if(prefecture['value']=="13"):
-					file=open('txt_files/'+prefecture.text+'.txt', 'w')
+					fPath='txt_files/%s.txt'%prefecture.text
+					
+					if not(os.path.exists(fPath)):
+						file=open(fPath, 'w')
+					else:
+						file=open(fPath, 'a')
 					html=requests.get('https://www.navitime.co.jp/async/category/addressList?addressCode=%s'%prefecture['value'])
 					js=json.loads(html.text)
 					for code in js:
 						page=1
 						while True:
 							#тут тоже не забудь поменять параметр после категории
+							#time.sleep(5)
 							body=getHTML('https://www.navitime.co.jp/category/%s/%s?page=%s'%(shop['value'], code['code'], page))
 							linkDiv=body.find('div', {'id':'spot_list'})
 							linkDl=linkDiv.findChildren('dl', {'class':'list_item_frame'})
@@ -50,7 +62,7 @@ for shopType in shopTypes:
 							else:
 								break
 						print('%s has done'%code['name'])
-file.close()
+					file.close()
 print('%s has done'%prefecture.text)
 print('All done, %s records writed'%recordCount)
 endTime=datetime.now()
