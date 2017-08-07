97ea32ade35867805e70c1ef5ee0eae30a3ae950
gin <suigintounotsuikaima@gmail.com>
2017-08-07 18:40:22 +0900

    added main parse module and ignore file

--- mainParse.py
+++ mainParse.py
@@ -1,5 +1,5 @@
 
-# -*- coding: utf-8 -*
+# -*- coding: utf-8 -*-
 
 import requests
 import json
@@ -28,6 +28,7 @@ shopTypes=['0201', '0202']
 #тут менять параметр после категории для перехода по магазинам/конбини
 #выдирать значения можно непосредственно из селектов
 #0201 - конбини
+
 startTime=datetime.now();
 recordCount=0
 for shopType in shopTypes:
@@ -44,11 +45,6 @@ for shopType in shopTypes:
 			for prefecture in prefList:
 				#сейчас только Токио, поэтому только 13
 				if(prefecture['value']=="13"):
-					#fPath='txt_files/%s.txt'%prefecture.text
-					#if not(os.path.exists(fPath)):
-					#	file=open(fPath, 'w')
-					#else:
-					#	file=open(fPath, 'a')
 					file=openFile('txt_files/%s.txt'%prefecture.text)
 					html=requests.get('https://www.navitime.co.jp/async/category/addressList?addressCode=%s'%prefecture['value'])
 					js=json.loads(html.text)
@@ -66,7 +62,7 @@ for shopType in shopTypes:
 									link=dl.find('a')
 									name=link.text
 									addr=dl.find('dd').contents[0]
-									file.write('%s\n%s\n'%(name.encode('utf-8'), addr.encode('utf-8')))
+									file.write('%s\n%s\n%s\n'%(shop['value'].encode('utf-8'), name.encode('utf-8'), addr.encode('utf-8')))
 									recordCount=recordCount+1
 							else:
 								break
@@ -76,3 +72,37 @@ print('%s has done'%prefecture.text)
 print('All done, %s records writed'%recordCount)
 endTime=datetime.now()
 print('It taked %s'%str(endTime-startTime))
+
+print('Start db inserting')
+
+from mainDb import DB
+
+db=DB('ini/connect.ini')
+db.connect();
+
+queryString=[]
+file=open('txt_files/shops.txt', 'r')
+i=1
+for str in file:
+	queryString.append(str[:-1])
+	if(i<2):
+		i=i+1
+	else:
+		db.executeQuery('INSERT INTO shops(id, name) VALUES(%s, "%s")'%(queryString[0], queryString[1]), 2)
+		i=1
+		del queryString[:]
+file.close()
+
+
+file=open('txt_files/東京都.txt', 'r')
+i=1
+for str in file:
+	queryString.append(str[:-1])
+	if(i<3):
+		i=i+1
+	else:
+		db.executeQuery('INSERT INTO branches(shop_id, name, address) VALUES(%s, "%s", "%s")'%(queryString[0], queryString[1], queryString[2]), 2)
+		i=1
+		del queryString[:]
+file.close()
+db.closeConnection()
